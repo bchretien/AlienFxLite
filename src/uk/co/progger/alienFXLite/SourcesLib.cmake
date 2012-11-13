e19c1953ce2d2bc27ac145391e45ab5280aa6077
Benjamin Chrétien <chretien@lirmm.fr>
2012-11-13 23:19:22 +0000

    Generate jar file thanks to CMake

---
+++ src/uk/co/progger/alienFXLite/SourcesLib.cmake
@@ -0,0 +1,46 @@
+SET(currentFolder "src/uk/co/progger/alienFXLite")
+
+SET(JAVA_ALIENFX_SRC
+  ${currentFolder}/AlienFXProperties.java
+  ${currentFolder}/JarClassLoader.java
+  ${currentFolder}/Launcher.java
+  ${currentFolder}/Main.java
+  ${currentFolder}/alienfx/AlienFXAction.java
+  ${currentFolder}/alienfx/AlienFXActionBlink.java
+  ${currentFolder}/alienfx/AlienFXActionColor.java
+  ${currentFolder}/alienfx/AlienFXActionMorph.java
+  ${currentFolder}/alienfx/AlienFXEngine.java
+  ${currentFolder}/alienfx/AlienFXProfile.java
+  ${currentFolder}/alienfx/AlienFXProfiles.java
+  ${currentFolder}/alienfx/AlienFXProfileSetting.java
+  ${currentFolder}/gui/ActionClipboard.java
+  ${currentFolder}/gui/ActionPanel.java
+  ${currentFolder}/gui/ActionPanelFX.java
+  ${currentFolder}/gui/AddActionPanel.java
+  ${currentFolder}/gui/AlienFXAlreadyRunningException.java
+  ${currentFolder}/gui/AlienFXLiteGUIConstants.java
+  ${currentFolder}/gui/AlienFXResources.java
+  ${currentFolder}/gui/AlienFXTexts.java
+  ${currentFolder}/gui/ColorBasePanel.java
+  ${currentFolder}/gui/ColorChooserPanel.java
+  ${currentFolder}/gui/ColorModel.java
+  ${currentFolder}/gui/ColorPanel.java
+  ${currentFolder}/gui/ColorUsedPanel.java
+  ${currentFolder}/gui/DashedBorder.java
+  ${currentFolder}/gui/DropDownButton.java
+  ${currentFolder}/gui/JXTrayIcon.java
+  ${currentFolder}/gui/MainFrame.java
+  ${currentFolder}/gui/ProfileModel.java
+  ${currentFolder}/gui/ProfilePanel.java
+  ${currentFolder}/gui/ProfileSelectionPanel.java
+  ${currentFolder}/gui/RegionPanel.java
+  ${currentFolder}/led/AlienFXAllPowerFull.java
+  ${currentFolder}/led/AlienFXArea51Controller.java
+  ${currentFolder}/led/AlienFXCommunicationException.java
+  ${currentFolder}/led/AlienFXController.java
+  ${currentFolder}/led/AlienFXControllerFactory.java
+  ${currentFolder}/led/AlienFXControllerNotFoundException.java
+  ${currentFolder}/led/AlienFXPowerMode.java
+  ${currentFolder}/led/AlienFXRegion.java
+  ${currentFolder}/led/LEDController.java
+)
