e48a83a3ae9f6f6c62e0a77b37386886e58ee262
Benjamin Chrétien <chretien@lirmm.fr>
2015-09-03 09:28:55 +0900

    Prevent any wrong call to install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 # You can pass CMake arguments to the install script
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+pushd ${DIR}
 
 rm -rf build
 mkdir -p build && cd build
 cmake .. $@
 make && make install
-cd ..
+
+popd
